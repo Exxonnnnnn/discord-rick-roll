50ab8f34cabd3434aa913b50e42a4ec70f785671
Exxon <tomfagan03@gmail.com>
2021-03-14 18:33:15 +0000

    Initial Commit :)

---
+++ README.md
@@ -0,0 +1,93 @@
+<h1 style="text-align:center">Discord-Rick-Roll</h1>
+
+
+<div style="text-align:center">
+<a href="https://discord.com/invite/jUNbV5u"><img src="https://img.shields.io/discord/769710808435261490.svg"></a>
+<a href="https://www.npmjs.com/package/discord-rick-roll"><img src="https://img.shields.io/npm/dt/discord-rick-roll.svg"></a>
+<a href="https://www.npmjs.com/package/discord-rick-roll"><img src="https://img.shields.io/npm/dm/discord-rick-roll.svg?style=color=blue"></a>
+<a href="https://www.npmjs.com/package/discord-rick-roll"><img src="https://img.shields.io/npm/v/discord-rick-roll.svg?style=color=blue"></a>
+<a href="https://github.com/Exxonnnnnn/discord-rick-roll"><img src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square"></a>
+</div>
+
+<br>
+
+# Contents
+- [Installation](#installation)
+- [Setup](#setup)
+    - [Create](#create)
+    - [Eval](#eval)
+    - [Rick](#rick)
+    - [Astley](#astley)
+    - [Cannon](#cannon)
+    - [Agred](#agred)
+- [Other](#other)
+
+<br>
+
+# Installation
+```
+npm i discord-rick-roll --save
+```
+
+<br>
+
+# Setup
+```js
+// Top of the command file
+const Rick = require('discord-rick-roll');
+const roll = new Rick();
+```
+
+<br>
+
+## Create
+```js
+roll.create({ msg: message })
+```
+
+<br>
+
+## Eval
+```js
+roll.eval({ msg: message, input: yourInput })
+```
+
+<br>
+
+## Google
+```js
+roll.google({ msg: message, query: yourQuery })
+```
+
+<br>
+
+## Rick
+```js
+roll.rick({ msg: message })
+```
+
+<br>
+
+## Astley
+```js
+roll.astley({ msg: message })
+```
+
+<br>
+
+## Cannon
+```js
+roll.cannon({ msg: message })
+```
+
+<br>
+
+## Agred
+```js
+roll.agred({ msg: message })
+```
+
+<br>
+
+# Other
+Please don't take this package seriously, but feel free to use it. You might enjoy it :)
---
+++ src/index.js
@@ -0,0 +1,102 @@
+const colors = require('colors');
+const { MessageEmbed } = require('discord.js');
+
+const embed = new MessageEmbed()
+.setColor('#00DCFF')
+
+class Rick {
+
+    create({ msg }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+
+        embed
+        .setTitle('Successfully created the video')
+        .setDescription('You can find the video [here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)')
+        
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+    eval({ msg, input }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+
+        if (!input)
+            input = 'Absolutely nothing because you failed to give me something to evaluate';
+
+        embed
+        .setTitle('Evaluating.....')
+        .addFields(
+            { name: 'Input', value: `\`\`\`${input}\`\`\``, inline: false },
+            { name: 'Output', value: `|| [You are an idiot](https://www.youtube.com/watch?v=dQw4w9WgXcQ) ||`, inline: false }
+        )
+        
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+    google({ msg, query }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+        
+        embed
+        .setTitle(`Searching google for ${query}`)
+        .setDescription('Result - [Click Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)')
+
+        msg.channel.send(msg.author, { embed: embed });
+    }
+
+    rick({ msg }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+
+        embed
+        .setDescription('[Click Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)');
+        
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+    astley({ msg }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+
+        embed
+        .setDescription('[Click Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)');
+
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+    cannon({ msg }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+        
+        embed
+        .setTitle('Who is cannon?')
+        .setDescription('Well Cannon is a person. Cannon is Portugeese and likes TypeScript, JavaScript (Not rlly) and Firebase. Cannon is learning C++, HTML and English. Cannon is also the creator of [Valex Bot](https://github.com/CannonStealth/valex)')
+        .addFields(
+            { name: 'Advantages of Cannon', value: '[Speaks Portugeese](https://www.youtube.com/watch?v=dQw4w9WgXcQ)', inline: true },
+            { name: 'Disadvantages of Cannon', value: '[Idk](https://www.youtube.com/watch?v=dQw4w9WgXcQ)', inline: true },
+            { name: 'GitHub', value: `[Click Here](https://github.com/CannonStealth)`, inline: true },
+        )
+        
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+    agred({ msg }) {
+        if (!msg)
+            return console.log(`${colors.brightRed('[YIKES]')}`.white + colors.white(' You can\'t even give me a proper message? Smh'));
+
+        embed
+        .setTitle('Who is Agred?')
+        .setDescription('Agred is the creator of [MegaFlushed Bot](https://discord.gg/q9yJh3qFUG). He is also known as Acery, whichever you prefer')
+        .addFields(
+            { name: 'Advantages of Agred', value: '[Says **GOTEEEM**](https://www.youtube.com/watch?v=dQw4w9WgXcQ)', inline: true },
+            { name: 'Disadvantages of Agred', value: '[Kinda sus](https://www.youtube.com/watch?v=dQw4w9WgXcQ)', inline: true },
+            { name: 'GitHub', value: `[Click Here](https://github.com/GG101xAcery)`, inline: true }
+        )
+        
+        msg.channel.send(msg.author, { embed: embed });
+    };
+
+}
+
+module.exports = Rick;
